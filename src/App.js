cdf78a4c2d7d388b0457e37dea193700ba9c2e64
adesob <55300413+adesob@users.noreply.github.com>
2021-12-25 00:38:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/* Loading Page */}
+      <div class = "w-screen h-screen bg-slate-900">
+
+        <div class = "m-auto max-w-2xl"> 
+          <h1 id = "title">Gr- 
+            <span class = "inline-flex">
+                <img src = "/images/food.png"  ></img>
+            </span>
+            b
+          </h1>
+        
+        </div>
+
+      </div>
     </div>
   );
 }
