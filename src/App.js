c85b35ff7c09165423fa59f02b71c7657d449b47
adesob <55300413+adesob@users.noreply.github.com>
2021-12-25 01:26:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       {/* Loading Page */}
-      <div class = "w-screen h-screen bg-black flex items-center">
+      <div class = "w-screen h-screen bg-zinc-900 flex items-center">
 
-        <div class = "mx-auto border border-red-700 flex flex-col items-center"> 
-          <p id = "title" class = "text-8xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-fuchsia-600">GR-UH
+        <div class = "mx-auto  flex flex-col items-center"> 
+          <p id = "title" class = "tracking-tighter text-8xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-fuchsia-700">gr-uhh
             {/* <span class = "inline-flex self-center">
                 <img src = "/images/food.png"  class = "h-8 w-9"></img>
             </span> */}
-            -B
+            -b
           </p>
 
-          <p id = 'title-2' class= "mx-auto mt-2 text-2xl text-white">Hungry are we?</p>
+          <p id = 'title-2' class= "mx-auto mt-1 text-xl text-neutral-300">Hungry are we?</p>
         
         </div>
 
