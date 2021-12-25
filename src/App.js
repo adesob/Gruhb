916267d6f4a2df0b53288a86c2f6968580b535b7
adesob <55300413+adesob@users.noreply.github.com>
2021-12-25 03:26:59 -0400

    Splash Page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {useState, useEffect} from 'react'
 import './App.css';
+import SplashPage from'./Components/SplashPage'
+import NavBar from'./Components/NavBar'
 
 function App() {
   const [show, setShow] = useState(true)
@@ -19,30 +21,14 @@ function App() {
   }, []);
   
 
-  const SplashPage = () => {
-   return(
-    <div class = "w-screen h-screen bg-zinc-900 flex items-center">
-
-          <div class = "mx-auto flex flex-col items-center"> 
-            <p id = "title" class = "animate-pulse tracking-tighter text-8xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-fuchsia-700">GRUH
-              {/* <span class = "inline-flex self-center">
-                  <img src = "/images/food.png"  class = "h-8 w-8"></img>
-              // </span> */}
-              B
-            </p>
-
-            <p id = 'title-2' class= "mx-auto mt-1 text-xl text-neutral-300 tracking-widest">Hungry are we?</p>
-          
-          </div>
-
-        </div>
-   )
-  }
+  
 
   return (
     <div className="App">
+      
       {/* Loading Page */}
       {show && <SplashPage/>} 
+      {!show && <NavBar/>}
     </div>
   );
 
