75e1654ce9b5dbfc447585f9c5dceb50f2938ca6
adesob <55300413+adesob@users.noreply.github.com>
2021-12-25 00:43:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
       <div class = "w-screen h-screen bg-slate-900">
 
         <div class = "m-auto max-w-2xl"> 
-          <h1 id = "title">Gr- 
+          <h1 id = "title" class = "mx-auto">Gr- 
             <span class = "inline-flex">
-                <img src = "/images/food.png"  ></img>
+                <img src = "/images/food.png"  class = ""></img>
             </span>
-            b
+            -b
           </h1>
+
+          <p id = 'title-2' class= "mx-auto">Hungry are we?</p>
         
         </div>
 
