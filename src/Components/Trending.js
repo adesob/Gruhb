f4ef5aeead468d22736b01f03d1e67bd51a2df97
adesob <55300413+adesob@users.noreply.github.com>
2022-01-12 23:32:57 -0400

    Update Trending.js

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -1,26 +1,62 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react'
 const Trending = () => {
+    
+    
+    const[trending, setTrending] = useState([])
+    
+    useEffect(() =>{
+
+        const getTrending = async () => {
+            const trendingPosts = await fetchTrending(); 
+            setTrending(trendingPosts.slice(0,8));
+            console.log('result', trending)
+        }
+
+        getTrending()
+        // fetchTrending()
+    }, [])
+    
+
+    const fetchTrending = async () => {
+        const res = await fetch("https://tasty.p.rapidapi.com/feeds/list?size=2&timezone=%2B0700&vegetarian=false&from=0", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "tasty.p.rapidapi.com",
+                "x-rapidapi-key": "4b2aa70226msh86a252cadbae61ep1ee408jsne2ce7af60c33"
+            }
+        });
+        const data = await res.json();
+        const output = await data.results[2].items
+        console.log(output)
+
+        return output
+    }
+
+
     return (
         <section id = "trending" class = "">
            <div class = "mx-auto my-4 sm: max-w-4xl px-7">
-               <p class = "text-3xl font-black">trending</p>
+                <p class = "text-3xl font-black"> what's trending?</p>
 
-               <div class = "flex justify-between  space-x-10  my-3 w-full rounded-md border shadow-sm">
-        
-                    <img src = "/images/chocchip.jpg" class = "h-60 "></img>
-                
-
-                    <div class = "py-7 pr-12 flex flex-col space-y-4">
-                        <p class = "text-3xl font-bold">chocolate chip cookies </p>
-                        <p class = "text-xl">there are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-                    </div>
-                
-
-               </div>
+                <div class = "flex flex-wrap my-3 w-full">
+                    {trending.map((post) => (
+                        <TrendingPost img = {post.thumbnail_url} title = {post.name} id = {post.id}/>
+                    ))}
+                    
+                </div>
 
            </div>
         </section>
+    )
+}
+
+const TrendingPost = ({img, title, id}) => {
+    return(
+        <div className = "group flex flex-col w-20 sm:w-48 sm:mr-4 mb-2">
+            <img src = {img} class = "h-24 w-24 rounded-md sm:h-48 sm:w-48 group-hover:scale-105 transition duration-500 group-hover:-translate-y-2 shadow-md group-hover:drop-shadow-xl"></img>
+            <p class = "text-lg ml-1 my-2 font-semibold leading-tight group-hover:text-orange-500 transition duration-300">{title}</p>
+        </div>
     )
 }
 
